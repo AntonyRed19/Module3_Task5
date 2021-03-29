77a61c67baf45aaffa29ac063153d13c0e201d5e
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-03-29 12:54:33 +0300

    added empty project

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

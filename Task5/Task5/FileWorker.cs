c2493c5e75c3ddf2d9b7b0a32fdf558005483f5c
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-01 13:30:10 +0300

    deleted extra async and await

--- Task5/Task5/FileWorker.cs
+++ Task5/Task5/FileWorker.cs
@@ -12,43 +12,18 @@ namespace Task5
         private readonly string _path = "1.txt";
         private readonly string _text = "Hello";
         private readonly string _text1 = "World";
-
         public void CreateFile()
         {
             File.WriteAllText(_path, _text);
         }
 
-        public async Task<string> HelloAsync()
-        {
-            var a = File.ReadAllTextAsync(_path);
-            return await a;
-        }
-
-        public async Task<string> WorldAsync()
-        {
-            return await Task.FromResult(_text1);
-        }
-
-        public async Task ListofAsync()
-        {
-             await HelloAsync();
-             await WorldAsync();
-             await Task.Run(() => _text + _text1);
-        }
-
         public async Task<string> ListofMethodsAsync()
         {
-            var list = new List<Task>();
-            list.Add(Task.Run(async () =>
-            {
-                return await File.ReadAllTextAsync(_path);
-            }));
-            list.Add(Task.Run(async () =>
-            {
-                return await Task.FromResult(_text1);
-            }));
-            await Task.WhenAll(list);
-            return await Task.Run(() => _text + _text1);
+            var list = new List<Task<string>>();
+            list.Add(Task.Run(() => File.ReadAllTextAsync(_path)));
+            list.Add(Task.Run(() => _text1));
+            var result = await Task.WhenAll(list);
+            return string.Join(" ", result);
         }
     }
 }
